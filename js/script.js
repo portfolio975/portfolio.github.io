86d8332438581a39a767b6f17a47504d51be0b23
portfolio975 <109471362+portfolio975@users.noreply.github.com>
2025-07-22 22:50:11 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -33,3 +33,62 @@ jQuery(window).on("scroll", function() {
     jQuery(".c-to-top-button").css("opacity", "0");
   }
 });
+
+// スクロール連動
+$(window).on("scroll", function () {
+  $(".l-main section").each(function () {
+    var sectionTop = $(this).offset().top;
+    var sectionHeight = $(this).outerHeight();
+    var scrollPos = $(window).scrollTop();
+    var sectionId = $(this).attr("id");
+
+    if (scrollPos >= sectionTop - 50 && scrollPos < sectionTop + sectionHeight - 100) {
+      $(".p-header__nav-link").removeClass("active");
+      $('.p-header__nav-link[href="#' + sectionId + '"]').addClass("active");
+    }
+  });
+});
+
+
+
+
+// const intersectionObserver = new IntersectionObserver(function(entries) {
+//   entries.forEach(function(entry) {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("is-in-view");
+//     } else {
+
+//     }
+//   })
+// });
+
+// const inViewItems = document.querySelectorAll(".js-in-view");
+// inViewItems.forEach(function(inViewItem) {
+//   intersectionObserver.observe(inViewItem);
+// });
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const targets = document.querySelectorAll(".p-fade__in-up");
+
+  const options = {
+    root: null, // ビューポート
+    rootMargin: "-50% 0px -50% 0px", // 上下の中央50%の範囲がトリガー
+    threshold: 0, // マージンに入った瞬間に発火
+  };
+
+  const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-in-view");
+        observer.unobserve(entry.target); // 一度だけアニメーションしたい場合
+      }
+    });
+  }, options);
+
+  targets.forEach((target) => {
+    observer.observe(target);
+  });
+});
